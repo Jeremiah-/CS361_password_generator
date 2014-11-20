4c5c8f63044653829e7d52f0583d2324685b0400
Kaelin Hooper <kaelin@cs.utexas.edu>
2014-11-19 22:38:26 -0600

    successfully creating followers table

--- Passwords.java
+++ Passwords.java
@@ -1,11 +1,14 @@
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
+import java.util.Random;
+import java.lang.StringBuilder;
 
 public class Passwords {
 
-
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException {
 
 		if (args.length < 3) {
 			System.out.println("Error: not enough arguments given. Format should be \'Passwords reference-Filename N k\'");
@@ -18,10 +21,10 @@ public class Passwords {
 		String referenceTextFileName = args[0];
 
 		// the number of passwords to generate
-		String n = args[1];
+		int n = Integer.parseInt(args[1]);
 
 		// the length of the passwords
-		String k = args[2];
+		int k = Integer.parseInt(args[2]);
 
 		File referenceTextFile;
 
@@ -49,25 +52,131 @@ public class Passwords {
 			String word = fileReader.next().toLowerCase();
 			boolean isStartOfWord = true;
 			// treat '-' as two seperate words. other characters just ignore and treat as one word.
-			for (int i = 0; i < word.length(); i++) {
-				int letterASCIIVal = word.charAt(i) - 97;
-				int previousLetterVal;
 
-				if (letterASCIIVal > 0 && letterASCIIVal < 26 || letterASCIIVal == -52) {
-					if (isStartOfWord) {
-						// TODO: update the follwers table
-						
-					} else if (letterASCIIVal == -52) {
-						counts[i - 1]--;
+			int previousLetterVal = -1;
+			for (int i = 0; i < word.length() - 1; i++) {
 
-					} else {
-						// TODO: update like regular letter
+				int letterIndexVal = (int)word.charAt(i) - 97;
+
+				if (letterIndexVal >= 0 && letterIndexVal < 26) {
+
+					// count the total number of times a
+					// letter (excluding end letters) occurs
+					if (i != word.length() - 1) {
+						counts[letterIndexVal] += 1;
 					}
-				} else {
-					// ignore the char
+
+					if (isStartOfWord) {
+						// record occurrence of starting letter
+						starters[letterIndexVal] += 1;
+						isStartOfWord = false;
+					} else {
+						followers[previousLetterVal][letterIndexVal] += 1;
+					}
+
+					previousLetterVal = letterIndexVal;
+					isStartOfWord = (i == word.length() - 1) ? true : false;
+
 				}
+
+				// ignore the char
+
+			}
+		}
+
+		writeFollowersTable(followers);
+		StringBuilder passwords = new StringBuilder();
+
+		for (int num = 0; num < n; ++num) {
+			Random randGen = new Random(); // needs to be changed ***
+			int seed = randGen.nextInt(100);
+
+			// get starting letter for this password
+			char startingLetter = getStartingLetter(seed, starters);
+			passwords.append(" " + startingLetter);
+
+			// get intermediate and end letters for this password
+			for (int pChar = 0; pChar < k; ++pChar) {
+				seed = randGen.nextInt(100); // needs to be changed ***
+				char intermediateLetter = getIntermediateLetter(seed, followers, startingLetter);
+				passwords.append(intermediateLetter);
 			}
 
+			passwords.append("\n");
 		}
+
+		System.out.println("Passwords are:");
+		System.out.print(passwords.toString());
+
 	}
+
+
+	static char getStartingLetter(int seed, int[] starters) {
+		return 'a'; // change ***
+	}
+
+
+
+	static char getIntermediateLetter(int seed, int[][] followers, char startingLetter) {
+		int sum = 0;
+		int startingLetterIndex = (int)startingLetter - 97;
+
+		for (int i = 0; i < followers[0].length; ++i) {
+			sum += followers[startingLetterIndex][i];
+
+			if (sum > seed) {
+				// return char
+			}
+		}
+
+		return 'a'; // change ***
+	}
+
+
+	static void writeFollowersTable(int[][] followers) throws IOException {
+
+		// setup FileWriter
+		File followersTableFile;
+
+		try {
+			followersTableFile = new File("followersTable.txt");
+		} catch (NullPointerException e) {
+			System.out.println("Error: file name given is null.");
+			return;
+		}
+
+		followersTableFile.createNewFile();
+		FileWriter writer = new FileWriter(followersTableFile);
+
+
+		// print letters for column title
+		for (int i = 0; i < followers.length; ++i) {
+			char colLetter = (char)(i + 65);
+
+			if (i == 0) {
+				writer.write("            ");
+			}
+
+			writer.write(colLetter + "     ");
+		}
+
+		// print row titles and frequencies in followers table
+		for (int i = 0; i < followers.length; ++i) {
+			char rowLetter = (char)(i + 65); // letter for row title
+			writer.write("\n" + rowLetter + ":     ");
+
+			for (int j = 0; j < followers[0].length; ++j) {
+				String formattedString = String.format("%6d", followers[i][j]);
+				writer.write(formattedString);
+			}
+
+			writer.write("\n");
+		}
+
+		writer.flush();
+		writer.close();
+
+	}
+
+
 }
